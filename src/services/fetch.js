78c3e0959f8558ce95f3e304b1055973fca88e16
DrM3nt1r4s <marceliux@tutamail.com>
2021-04-20 02:09:23 -0600

    Changed URL to prod, environment variables? for what shall I need ya? jk :S

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,11 +1,11 @@
 //Prod
-// const url = `https://drsmaroto.com`
+const url = `https://drsmaroto.com`
 
 //staging
 // const url = `https://boiler-log-be.herokuapp.com`
 
 //dev
-const url = `http://192.168.100.34:3000`
+// const url = `http://192.168.100.34:3000`
 
 export const fetchRegister = (email, pwd, name, lastName, user) =>
 	fetch(`${url}/new`, {
