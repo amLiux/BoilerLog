11a16419a73172a2d29f1bc29d2b49b7e82d8391
Marcelo <marceliux@pop-os.localdomain>
2021-05-08 11:58:02 -0600

    Changed fetch url, thinking on creating an env var

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,11 +1,11 @@
-//Prod
-//const url = `https://drsmaroto.com`
+Prod
+const url = `https://drsmaroto.com`
 
 //staging
 // const url = `https://boiler-log-be.herokuapp.com`
 
 //dev
-const url = `http://localhost:3000`
+// const url = `http://localhost:3000`
 
 export const fetchRegister = (email, pwd, name, lastName, user) =>
 	fetch(`${url}/new`, {
